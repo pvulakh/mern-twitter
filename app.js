9f423f31ed39e89ee300db95b29a3d0d82692527
pvulakh <pvulakh@gmail.com>
2019-08-03 17:23:21 -0400

    connect to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(db, { userNewParser: true })
+  .then(() => console.log('connected to mongodb!'))
+  .catch(err => console.log(err));
+
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`server's running on port ${port}`));
+app.listen(port, () => {});
 app.get('/', (req, res) => res.send('hello from the mern intro'));
