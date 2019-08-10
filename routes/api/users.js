bdb5e00c247f588725fab628d27f3fc966a8ba3e
Polina Vulakh <pvulakh@gmail.com>
2019-08-10 18:50:19 -0400

    add route to register users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
 
 router.get("/test", (req, res) => res.json({ msg: "users route testing!"}));
+
+router.post("/register", (req, res) => {
+  // check that the provided email doesn't already have an associated account
+  User.findOne({ email: req.body.email})
+    .then(user => {
+      if (user) {
+        return res.status(400).json({email: 'This email already has an associated account.'})
+      } else {
+        const newUser = new User({
+          handle: req.body.handle,
+          email: req.body.email,
+          password: req.body.password
+        })
+
+        // create encrypted password before saving user to db
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          })
+        })
+      }
+    })
+})
 
 module.exports = router;
