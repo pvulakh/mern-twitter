f89e2df256abc18042568fac8a6461b89675e927
Polina Vulakh <pvulakh@gmail.com>
2019-08-11 20:57:32 -0400

    add login functionality

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const mongoose = require('mongoose');
 const express = require('express');
-const app = express();
 const db = require('./config/keys').mongoURI;
 const users = require('./routes/api/users');
 const tweets = require('./routes/api/tweets');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 mongoose.connect(db, { useNewUrlParser: true })
-  .then(() => console.log('connected to mongodb!'))
-  .catch(err => console.log(err));
+.then(() => console.log('connected to mongodb!'))
+.catch(err => console.log(err));
 
+const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {});
 
-app.get('/', (req, res) => res.send('hello from the mern intro'));
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
 app.use('/api/users', users);
 app.use('/api/tweets', tweets);
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,9 +2,19 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 const User = require('../../models/User');
-
+const passport = require('passport');
+//"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVkNTBhN2M3YjFhZmVmNzM3MzQxODE5ZSIsImlhdCI6MTU2NTU3MDQ4OCwiZXhwIjoxNTY1NTc0MDg4fQ.RFvbs4A0Tu5fhGAkU4rIUgrcYN8S4lflGuvEyDztHxc"
 router.get("/test", (req, res) => res.json({ msg: "users route testing!"}));
+
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.json({ 
+    id: req.user.id,
+    handle: req.user.handle,
+    email: req.user.email
+  });
+})
 
 router.post("/register", (req, res) => {
   // check that the provided email doesn't already have an associated account
@@ -43,7 +53,18 @@ router.post('/login', (req, res) => {
 
     bcrypt.compare(password, user.password).then(match => {
       if (match) {
-        res.json({ msg: 'success!'});
+        const payload = { id: user.id, name: user.name };
+
+        jwt.sign(
+          payload, 
+          keys.secretOrKey, 
+          { expiresIn: 3600},
+          (err, token) => {
+            res.json({
+              success: true,
+              token: 'Bearer ' + token 
+            });
+          });
       } else {
         return res.status(400).json({ password: 'incorrect password!'});
       }
